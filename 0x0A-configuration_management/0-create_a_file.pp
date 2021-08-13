14393949ad08b40d3494b06a6bcbac7f5ec9bb46
ForeroAlexander <foreroalex26@gmail.com>
2021-08-13 16:10:03 -0500

    create file puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
+#created file
 file {'/tmp/holberton':
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
