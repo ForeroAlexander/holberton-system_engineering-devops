942a88316bca813ad8693830f0913dfbf6292948
ForeroAlexander <foreroalex26@gmail.com>
2021-09-13 23:58:21 -0500

    fix task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,25 +7,28 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    user_id = argv[1]
+    USER_ID = argv[1]
 
     url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'
     url_user = 'https://jsonplaceholder.typicode.com/users/{}'
-    resp_todos = requests.get(url_todo.format(user_id))
-    resp_user = requests.get(url_user.format(user_id))
+    resp_todos = requests.get(url_todo.format(USER_ID))
+    resp_user = requests.get(url_user.format(USER_ID))
     json_r_todos = resp_todos.json()
     json_r_user = resp_user.json()
 
-    user_name = json_r_user.get("username")
+    USERNAME = json_r_user.get("username")
 
-    csv_fname = user_id + ".csv"
+    csv_fname = USER_ID + ".csv"
 
     with open(csv_fname, 'w+') as fd_csv:
 
         # Object used to write in CSV format
         csv_writer = csv.writer(fd_csv, quoting=csv.QUOTE_ALL)
 
-        for task in json_r_todos:
-            csv_writer.writerow([user_id,
-                                user_name,
-                                task['completed'], task['title']])
+        for todo in json_r_todos:
+                TASK_COMPLETED_STATUS = todo.get("completed")
+                TASK_TITLE = todo.get('title')
+                csv_writer.writerow([USER_ID,
+                                    USERNAME,
+                                    TASK_COMPLETED_STATUS,
+                                    TASK_TITLE])
