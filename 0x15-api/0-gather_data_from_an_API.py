f76b055f0938f5f8fb0b811062d81f9a6f7a8447
ForeroAlexander <foreroalex26@gmail.com>
2021-09-13 23:31:09 -0500

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-"""using this REST API, for a given employee ID, returns
-information about his/her TODO list progress.
-"""
+"""Is a Python script that, using this REST API, for a given=employee
+ID, returns information about his/her TODO list progress."""
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    user_id = argv[1]
+
     NUMBER_OF_DONE_TASKS = 0
     TASK_TITLE = []
 
-    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'
-    url_user = 'https://jsonplaceholder.typicode.com/users/{}'
-    resp_todos = requests.get(url_todo.format(user_id))
-    resp_user = requests.get(url_user.format(user_id))
-    json_r_todos = resp_todos.json()
-    json_r_user = resp_user.json()
+    request = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                           format(argv[1])).json()
 
-    if json_r_user and json_r_todos:
-        EMPLOYEE_NAME = json_r_user.get('name')
-        TOTAL_NUMBER_OF_TASKS = len(json_r_todos)
-        NUMBER_OF_DONE_TASKS = sum(item.get("completed")
-                                   for item in json_r_todos if item)
+    EMPLOYEE_NAME = request.get("name")
 
-        print("Employee {} is done with tasks({}/{}):"
-              .format(EMPLOYEE_NAME,
-                      NUMBER_OF_DONE_TASKS,
-                      TOTAL_NUMBER_OF_TASKS))
-        for todo in json_r_todos:
-            TASK_TITLE = todo.get('title')
-            if todo.get("completed"):
-                print("\t {}".format(TASK_TITLE))
+    request = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".
+        format(argv[1])).json()
+
+    for i in request:
+        if i.get("completed") is True:
+            TASK_TITLE.append(i.get("title"))
+            NUMBER_OF_DONE_TASKS += 1
+    TOTAL_NUMBER_OF_TASKS = len(request)
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for task in TASK_TITLE:
+        print("\t {}".format(task))
