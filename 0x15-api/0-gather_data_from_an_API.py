383d7efce81d2fbdb509744190604bbf66c1a254
ForeroAlexander <foreroalex26@gmail.com>
2021-09-13 23:20:10 -0500

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,6 +7,8 @@ from sys import argv
 
 if __name__ == "__main__":
     user_id = argv[1]
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
 
     url_todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'
     url_user = 'https://jsonplaceholder.typicode.com/users/{}'
@@ -17,8 +19,10 @@ if __name__ == "__main__":
 
     EMPLOYEE_NAME = json_r_user.get("name")
 
-    TOTAL_NUMBER_OF_TASKS = len(json_r_todos)
-    TASK_TITLE = list(filter(lambda d: d['completed'] is True, json_r_todos))
+    for i in resp_todos:
+        if i.get("completed") is True:
+                TOTAL_NUMBER_OF_TASKS = len(json_r_todos)
+    TASK_TITLE.append(i.get("title"))
     NUMBER_OF_DONE_TASKS = len(TASK_TITLE)
 
     print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
@@ -26,4 +30,4 @@ if __name__ == "__main__":
                                                           TOTAL_NUMBER_OF_TASKS))
 
     for dic in TASK_TITLE:
-        print("\t {}".format(dic.get("title")))
+        print("\t {}".format(dic.get("task")))
