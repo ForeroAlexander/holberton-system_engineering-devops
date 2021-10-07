cf95ab0c5002cb8b99b983e703dc7d39ea12a1f3
ForeroAlexander <foreroalex26@gmail.com>
2021-10-07 18:49:21 -0500

    Task-0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+#  fix our stack so that we get to 0
+#  and remember that when something is going wrong.
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
